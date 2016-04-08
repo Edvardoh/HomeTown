4a08ab5758e38260a8b61ed9cfa71b85e5693cd2
Tom DeHart <tdehart@gmail.com>
2016-04-08 16:34:32 -0400

    React boilerplate and map widget

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import GettingStarted from './GettingStarted';
+
+export default class App extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Hello world</h1>
+        <GettingStarted/>
+      </div>
+    );
+  }
+}
